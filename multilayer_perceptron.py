1e0ba8435204b49d9037e7dd025506764ca29d6d
Brian Ail <ail.brianez@gmail.com>
2020-05-14 18:01:05 -0300

    Multi layer perceptron funcionandogit status

--- multilayer_perceptron.py
+++ multilayer_perceptron.py
@@ -7,82 +7,120 @@ def print_size(m, name):
 class Multilayer_perceptron:
 
     def __init__(self, layers, data_attributes):
-        # Layers is an array that contains the amount of layers (length), and the amount 
-        # of nodes in each layer. For example:
-        # layers => [3,2] means we have a hidden layer of size 3 and an output layer of size 2
+        # Initialization of arrays because I dont know python
         self.layers = layers
         self.weights = [None] * (len(layers))
+        self.bias = [None] * (len(layers))
         self.layer_outputs = [None] * (len(layers))
         self.layer_activations = [None] * (len(layers))
         self.deltas = [None] * (len(layers))
+        self.deltaW = [None] * (len(layers))
 
-        self.learning_rate = 1
 
+        # Network constants
+        self.learning_rate = 1.3
+        self.beta = 0.5
+        self.max_epochs = 1000000
+        self.error_threshold = 1e-5
+        self.batch = False
         # Initializing random weights
         # TODO: Check if random.random is a good weight initialization method
         for i in range(len(layers)):
             l_out = layers[i]
             l_in  = data_attributes if i == 0 else layers[i-1]
-            self.weights[i] = np.random.rand(l_in,l_out)
-        
-        # print(self.weights)
+            self.weights[i] = np.random.randn(l_in+1,l_out) * 0.01
+            self.bias[i] = np.ones((l_out,1))
+
     
     def train_weights(self, data, expected):
-        n_epochs = 100
-        for epoch in range(n_epochs):
-            self.forward(data)        
-            self.back_prop(data, expected)
+        print(self)
+        self.error = self.error_threshold + 1
+        for epoch in range(self.max_epochs):
+            if self.batch:
+                self.forward(row)        
+                self.back_prop(row, result)
+            else:
+                for sample,result in zip(data,expected):
+                    row = np.array([sample])
+                    self.forward(row)        
+                    self.back_prop(row, result)
+
+            if self.error < self.error_threshold:
+                print("Broken in {} iterations because error is {}".format(epoch, self.error_threshold))
+                break
+
+        print("ERROR: {}".format(self.error))
+
+    def __str__(self):
+        r = ""
+        r += "------ Multi layer perceptron ----------\n"
+        for i in range(len(self.layers)):
+            if i == len(self.layers) -1:
+                r += "Output layer size:{}\n".format(self.layers[i])
+            else:
+                r += "Hidden layer {} size:{}\n".format(i, self.layers[i])
+        
+        r += "Beta:{}\n".format(self.beta)
+        r += "Learning rate:{}\n".format(self.learning_rate)
+        r += "Maximum epoch:{}\n".format(self.max_epochs)
+        r += "Error threshold:{}\n".format(self.error_threshold)
+        return r
         
     def predict(self, data):
         for i in range(len(self.layers)):
             layer_input = data if i == 0 else self.layer_activations[i - 1]
-            self.layer_outputs[i] = layer_input.dot(self.weights[i])            
+            layer_input = np.column_stack((layer_input, np.ones((len(layer_input),1))))
+            self.layer_outputs[i] = layer_input.dot(self.weights[i])
             self.layer_activations[i] = self.sigmoid(self.layer_outputs[i])
 
+        print(self.layer_activations[len(self.layer_activations) - 1])
         return self.layer_activations[len(self.layers) - 1] > 0.5
+
+
 
     # We expect data to already have the bias column added
     def forward(self, data):
         # Cicle through each layer
         for i in range(len(self.layers)):
             layer_input = data if i == 0 else self.layer_activations[i - 1]
-            self.layer_outputs[i] = layer_input.dot(self.weights[i])            
+
+            layer_input = np.column_stack((layer_input, np.ones((len(layer_input),1))))
+            self.layer_outputs[i] = layer_input.dot(self.weights[i]) + self.bias[i].T
             self.layer_activations[i] = self.sigmoid(self.layer_outputs[i])
+        
 
     def back_prop(self, data, expected):
-        max_layer = len(self.layers) - 1
-        sum_error = expected - self.layer_activations[max_layer]
-        for i in range(max_layer, -1, -1):
+        output_layer = len(self.layers) - 1
+        error_vector = (expected - self.layer_activations[output_layer])
+        self.error = np.sum(error_vector**2)
+        for i in range(output_layer, -1, -1):
             # Casp especial para ultima y para primer layer:
-            prev_activation = data if i == 0 else self.layer_activations[i - 1]
-
-            if i == max_layer:
-                self.deltas[i] = -1 * sum_error * self.sigmoid_derivate(self.layer_outputs[i])
-            else:
-                self.deltas[i] = self.sigmoid_derivate(self.layer_outputs[i]) * (self.deltas[i+1].dot(self.weights[i+1].transpose()))
-            deltaW = self.learning_rate * np.transpose(prev_activation).dot(self.deltas[i])
-            self.weights[i] = self.weights[i] - deltaW
-
-    def sigmoid(self, layer_output):
-        return 1 / (1 + np.exp(-layer_output))
-
-    def sigmoid_derivate(self, m):
-        return self.sigmoid(m) * (1- self.sigmoid(m))
 
             
-# Forward propagation
+            curr_output = self.layer_outputs[i]
+
+            if i == output_layer:
+                self.deltas[i] = error_vector * self.sigmoid_derivate(self.layer_outputs[i])
+
+            else:
+                dt = (self.deltas[i+1].dot(self.weights[i+1].T))
+                # print_size(dt[:,:-1], "dt")
+                # print_size(curr_output, "curr_output")
+                self.deltas[i] = self.sigmoid_derivate(curr_output) * dt[:,:-1]
 
 
-# Training data:
-# size NxM where N is the amount of training samples and M is the amount of attributes per sample
+        for i in range(len(self.layers)):
+            prev_activation = data if i == 0 else self.layer_activations[i - 1]
+            prev_activation = np.column_stack((prev_activation, np.ones((len(prev_activation),1))))
+            self.deltaW[i] = self.learning_rate * ((prev_activation).T).dot(self.deltas[i])
 
-# Hidden layer of size 3
-# Weights: Each unit in the layer has an array of weights for each attribute of the previous layer's output
-# Weights size: Mx3
-# Output size: Nx3
+        # Actualizar los pesos
+        for i in range(output_layer, -1, -1):
+            self.weights[i] = self.weights[i] + self.deltaW[i]
+            # self.bias[i] = self.bias[i] + self.deltaB[i]
 
-# Output layer of size 1
-# Weights size: 3x1
-# Output: size Nx1
+    def sigmoid(self, layer_output):
+        return 1 / (1 + np.exp(-2*self.beta*layer_output))
 
-# Back propagation
+    def sigmoid_derivate(self, m):
+        return 2 * self.beta * (self.sigmoid(m) * (1 - self.sigmoid(m)))
