6ceb429cf745cb5e14643e4283acfd2d536449dd
Marina <mfuster@itba.edu.ar>
2020-05-15 09:55:06 -0300

    Reviews simple perceptron to make sure is implemented correctly

--- simple_perceptron.py
+++ simple_perceptron.py
@@ -2,36 +2,11 @@ import sys
 import numpy as np
 from graphics_plot import plot_and_or_xor
 
-
 def step_function(inputs, weights):
 	activation=0.0
 	for inp,weight in zip(inputs, weights): # each input corresponding to each weight
 		activation += inp*weight
-	return 1.0 if activation > 0.0 else -1.0
-
-
-def sigmoid(inputs, weights, L=100, beta=0.0001):
-	activation = 0.0
-	for inp, weight in zip(inputs, weights):  # each input corresponding to each weight
-		activation += inp * weight
-	return L / (1 + np.exp(-2*beta*activation))
-
-
-def sigmoid_derivate(inputs, weights, L=100, beta=0.0001):
-	return L * 2 * beta * (sigmoid(inputs, weights) * (1 - sigmoid(inputs, weights)))
-
-
-def tanh_activation(inputs, weights, L=100):
-	activation = 0.0
-	for inp, weight in zip(inputs, weights):  # each input corresponding to each weight
-		activation += inp * weight
-	return L*np.tanh(activation)
-
-def relu(inputs, weights):
-	activation = 0.0
-	for inp, weight in zip(inputs, weights):  # each input corresponding to each weight
-		activation += inp * weight
-	return activation if activation > 0.0 else 0.0
+	return 1.0 if activation >= 0.0 else -1.0
 
 # calculates how well our algorithm is doing
 # on classificating the training examples
@@ -43,50 +18,23 @@ def accuracy(matrix, weights, predict=step_function):
 	return num_correct/float(len(matrix))
 
 # training algorithm for sample
-def train_weights(matrix, weights, predict=step_function, iterations=100, learning_rate=1.00, plot=False, stop_early=True):
-	for iteration in range(iterations):
+def train_weights(matrix, weights, predict=step_function, epochs=100, learning_rate=1.00, plot=False, stop_early=True):
+
+	# Adds a column of ones, so that w0 becomes the weight representing the bias
+	matrix = np.column_stack((np.ones((len(matrix),1)), matrix))
+
+	for epoch in range(epochs):
 		current_accuracy = accuracy(matrix, weights)
 
+		# We do not need further training if we reached our max accuracy
 		if current_accuracy==1.0 and stop_early: break
         
 		for i in range(len(matrix)):
 			prediction = predict(matrix[i][:-1], weights) 				   # get predicted classificaion
 			error      = matrix[i][-1]-prediction		 			 	   # get error from real classification
-			for j in range(len(weights)): 				 				   # calculate new weight for each nodes
-				weights[j] = weights[j]+(learning_rate*error*matrix[i][j]) # update weights
+			for j in range(len(weights)):								   # calculate new weight for each nodes
+				weights[j] = weights[j]+(learning_rate*error*matrix[i][j]) # update weights w = w + deltaw
 
 	if plot:
 		plot_and_or_xor(matrix, predict, weights)
 	return weights
-
-
-# training algorithm for sample
-def train_weights_nonlinear(matrix, weights, predict=sigmoid, derive=sigmoid_derivate, iterations=100, learning_rate=1.00, stop_early=True):
-	for iteration in range(iterations):
-		current_accuracy = accuracy(matrix, weights, predict=predict)
-
-		if current_accuracy == 1.0 and stop_early: break
-
-		for i in range(len(matrix)):
-			prediction = predict(matrix[i][:-1], weights)  	# get predicted classificaion
-			derivative = derive(matrix[i][:-1], weights)	# get derivative of inputs*weights
-			error = matrix[i][-1] - prediction  # get error from real classification
-			for j in range(len(weights)):  # calculate new weight for each nodes
-				weights[j] = weights[j] + (learning_rate * error * derivative * matrix[i][j])  # update weights
-
-	return weights
-
-
-def test_perceptron(matrix, weights, predict=sigmoid, print_results=False):
-	predictions = []
-	groundtruths = []
-	RSEs = []
-	if print_results:
-		print('True values; Predicted values; Root Mean Errors')
-	for i in range(len(matrix)):
-		predictions.append(np.round(predict(matrix[i][:-1], weights), 2))
-		groundtruths.append(np.round(matrix[i][-1], 2))
-		RSEs.append(np.round(np.sqrt((predictions[i] - groundtruths[i])**2), 2)) #root square error
-		if print_results:
-			print('%f		%f		%f' % (groundtruths[i], predictions[i], RSEs[i]))
-	return [predictions, groundtruths, RSEs]
