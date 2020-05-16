9c36c411246ecf3d46400e04d685d148457dbf0d
Marina <mfuster@itba.edu.ar>
2020-05-16 11:56:31 -0300

    Modifications to data entry

--- main.py
+++ main.py
@@ -1,35 +1,30 @@
 from __future__ import print_function
 import sys
 import numpy as np
-from simple_perceptron import train_weights, step_function, sigmoid
+from simple_perceptron import train_weights, step_function
 from multilayer_perceptron import Multilayer_perceptron
 
 def main():
-
-	iterations		= 100
+	epochs		    = 100
 	learning_rate  	= 0.5
 	plot_iteration	= True
 	stop_early 		= True
 
-					# 	Bias 	x1 		x2 		y
-	and_matrix = [	[1.00,	1.00,	1.00,	1.0],
-					[1.00,	-1.00,	1.00,	-1.00],
-					[1.00,	1.00,	-1.00,	-1.00],
-					[1.00,	-1.00,	-1.00,	-1.00]]
+					# x1 	 x2       y
+	and_matrix = [	[1.00,	1.00,	1.0],
+					[-1.00,	1.00,	-1.00],
+					[1.00,	-1.00,	-1.00],
+					[-1.00,	-1.00,	-1.00]]
 	
-					# 	Bias 	x1 		x2 		y
-	xor_matrix = [	[1.00,	1.00,	1.00,	-1.0],
-					[1.00,	-1.00,	1.00,	1.00],
-					[1.00,	1.00,	-1.00,	1.00],
-					[1.00,	-1.00,	-1.00,	-1.00]]
+					# x1 	 x2       y
+	xor_matrix = [	[1.00,	1.00,	-1.0],
+					[-1.00,	1.00,	1.00],
+					[1.00,	-1.00,	1.00],
+					[-1.00,	-1.00,	-1.00]]
 
 	weights= [	 0.1,	0.30,  0.80		] # initial weights
 
-	train_weights(and_matrix, weights=weights, predict=step_function, iterations=iterations, learning_rate=learning_rate, plot=True, stop_early=stop_early)
-	#train_weights(xor_matrix, weights=weights, predict=sigmoid, iterations=iterations, learning_rate=learning_rate, plot=True, stop_early=stop_early)
-
-	
-
+	train_weights(and_matrix, weights=weights, predict=step_function, epochs=epochs, learning_rate=learning_rate, plot=True, stop_early=stop_early)
 
 def multi_layer():
 	# Multi layer perceptron
@@ -50,4 +45,4 @@ def multi_layer():
 
 if __name__ == '__main__':
 	main()
-	multi_layer()	
+	# multi_layer()	
